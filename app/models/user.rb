58a608fa59339c4bd4204ebde6e772059a876792
nicolas968 <nicolasbermudez666@gmail.com>
2018-08-31 09:29:57 -0300

    agrego restriccion de unicidad (#6)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
- has_many :user_habits
- has_many :user_characters
- has_many :user_categories
- has_many :habits, through: :user_habits
- has_many :categories, through: :user_categories
- has_many :characters, through: :user_characters
+	validates :nickname, uniqueness: true
+	validates :mail, uniqueness: true
+  has_many :user_habits
+  has_many :user_characters
+  has_many :user_categories
+  has_many :habits, through: :user_habits
+  has_many :categories, through: :user_categories
+  has_many :characters, through: :user_characters
 end
