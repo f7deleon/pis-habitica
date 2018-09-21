a18714b2d142ef792191e594b9532428ec667e27
Felipe de León <f7.deleon@gmail.com>
2018-09-21 19:10:01 -0300

    Added user_characters to seed (#41)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,12 @@
 #
 # Examples
 
-User.create([
-  { nickname: 'Feli', email: 'felipe@habitica.com', password: '12341234' },
-  { nickname: 'Pai', email: 'paiadmin@habitica.com', password: '12341234' },
-  { nickname: 'Lala', email: 'lala@habitica.com', password: '12341234' },
-  { nickname: 'Seba', email: 'sebastian@habitica.com', password: '12341234' },
-  { nickname: 'Leo', email: 'Leosqa@habitica.com', password: '12341234' },
-  { nickname: 'Santos', email: 'santos@habitica.com', password: '12341234' },
-  { nickname: 'Marco', email: 'marcopablo@habitica.com', password: '12341234' },
-  { nickname: 'Berna', email: 'bernardo@habitica.com', password: '12341234' }
-])
-
 DefaultType.create([
-	{name: 'Ejercicio', description: 'Hacer ejercicio'},
-	{name: 'Nutricion', description: 'Seguir determinada dieta'},
-	{name: 'Estudio', description: 'Estudiar por mas de 1 hora'},
-	{name: 'Social', description: 'Ir al bar'},
-	{name: 'Ocio', description: 'Jugar a la switch'}
+  {name: 'Ejercicio', description: 'Hacer ejercicio'},
+  {name: 'Nutricion', description: 'Seguir determinada dieta'},
+  {name: 'Estudio', description: 'Estudiar por mas de 1 hora'},
+  {name: 'Social', description: 'Ir al bar'},
+  {name: 'Ocio', description: 'Jugar a la switch'}
 ])
 
 Character.create([
@@ -34,3 +23,19 @@ Character.create([
   { name: 'Cazador', description: 'Siempre se acerca a su presa, la rodea y lanza el ataque.
                                    No titubea al momento de cumplir sus habitos.' }
 ])
+
+
+User.create([
+  { nickname: 'Feli', email: 'felipe@habitica.com', password: '12341234' },
+  { nickname: 'Pai', email: 'paiadmin@habitica.com', password: '12341234' },
+  { nickname: 'Lala', email: 'lala@habitica.com', password: '12341234' },
+  { nickname: 'Seba', email: 'sebastian@habitica.com', password: '12341234' },
+  { nickname: 'Leo', email: 'Leosqa@habitica.com', password: '12341234' },
+  { nickname: 'Santos', email: 'santos@habitica.com', password: '12341234' },
+  { nickname: 'Marco', email: 'marcopablo@habitica.com', password: '12341234' },
+  { nickname: 'Berna', email: 'bernardo@habitica.com', password: '12341234' }
+])
+
+User.all.each do |user|
+  UserCharacter.create(user_id: user.id, character_id: Character.order("RANDOM()").limit(1).first.id, creation_date: Time.zone.now, is_alive: true)
+end
