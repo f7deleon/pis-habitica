bffe47f3d64b0208e12d49af5c7a5a10bcc19c86
Leandro <ldrnuy@gmail.com>
2018-09-22 01:06:14 -0300

    Fix filtrar habitos activos en home (#44)

--- app/serializers/user_home_serializer.rb
+++ app/serializers/user_home_serializer.rb
@@ -3,7 +3,11 @@
 class UserHomeSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :nickname, :email
-  has_many :individual_habits
+
+  has_many :individual_habits do |object|
+    object.individual_habits.find_by_active(true)
+  end
+
   has_one :character do |object|
     object.user_characters.find_by_is_alive(true).character
   end
