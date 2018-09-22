9ad6231db8ae93aa512293ba4a4f0a52f0b28fb2
Felipe de León <f7.deleon@gmail.com>
2018-09-22 15:34:44 -0300

    Fix busqueda usuario envia el personaje vivo (#47)

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,4 +3,8 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :nickname, :email
+
+  has_one :character do |object|
+    object.user_characters&.find_by_is_alive(true)&.character
+  end
 end
