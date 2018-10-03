0eb889f0938bfa19701af6480725dd41e6aeb2ba
Felipe de León <f7.deleon@gmail.com>
2018-10-03 20:59:42 -0300

    Agregados test y quitados endpoints que no matchean el swagger (#77)

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -10,42 +10,10 @@ class TypesController < ApplicationController
     render json: TypeSerializer.new(types).serialized_json
   end
 
-  # GET /types/1
-  def show
-    render json: @type
-  end
-
-  # POST /types
-  def create
-    raise Error::ConflictError unless (type = Type.new(type_params))
-
-    render json: type, status: :created, location: @type
-  end
-
-  # PATCH/PUT /types/1
-  def update
-    if @type.update(type_params)
-      render json: @type
-    else
-      render json: @type.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /types/1
-  def destroy
-    @type.destroy
-  end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_type
     raise Error::NotFoundError unless (@type = Type.find(params[:id]))
-  end
-
-  # Only allow a trusted parameter "white list" through.
-  def type_params
-    params.require(:type).permit(:name, :description)
-    raise ActionController::ParameterMissing
   end
 end
--- test/controllers/types_controller_test.rb
+++ test/controllers/types_controller_test.rb
@@ -3,4 +3,31 @@
 require 'test_helper'
 
 class TypesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create(nickname: 'Example', email: 'example@example.com', password: 'Example123')
+    post '/user_token', params: {
+      'auth': {
+        'email': @user.email,
+        "password": @user.password
+      }
+    }
+    @user_token = JSON.parse(response.body)['jwt']
+
+    DefaultType.create([
+                         { name: 'Ejercicio', description: 'Hacer ejercicio' },
+                         { name: 'Nutricion', description: 'Seguir determinada dieta' },
+                         { name: 'Estudio', description: 'Estudiar por mas de 1 hora' },
+                         { name: 'Social', description: 'Ir al bar' },
+                         { name: 'Ocio', description: 'Jugar a la switch' }
+                       ])
+  end
+
+  test 'get Types' do
+    url = '/types'
+    result = get url, headers: { 'Authorization': 'Bearer ' + @user_token }
+    body = response.body
+    expected_response = TypeSerializer.new(DefaultType.all).serialized_json
+    assert result == 200
+    assert body == expected_response
+  end
 end
