fedd75aebe899f0ff2770d2b3b3943f3d985a901
Felipe de León <f7.deleon@gmail.com>
2018-11-03 19:53:52 -0300

    Agregados datos a las seeds (#138)

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,12 +27,14 @@ Character.create([
 User.create([
   { nickname: 'Feli', email: 'felipe@habitica.com', password: '12341234' },
   { nickname: 'Pai', email: 'paiadmin@habitica.com', password: '12341234' },
-  { nickname: 'Lala', email: 'lalah@abitica.com', password: '12341234' },
+  { nickname: 'Lala', email: 'lala@habitica.com', password: '12341234' },
   { nickname: 'Seba', email: 'sebastian@habitica.com', password: '12341234' },
   { nickname: 'Leo', email: 'Leosqa@habitica.com', password: '12341234' },
   { nickname: 'Santos', email: 'santos@habitica.com', password: '12341234' },
   { nickname: 'Marco', email: 'marcopablo@habitica.com', password: '12341234' },
-  { nickname: 'Berna', email: 'bernardo@habitica.com', password: '12341234' }
+  { nickname: 'Berna', email: 'bernardo@habitica.com', password: '12341234' },
+  { nickname: 'ElRodra', email: 'rodra@habitica.com', password: '12341234' },
+  { nickname: 'Mufasa', email: 'mufasa@habitica.com', password: '12341234'}
 ])
 
 User.all.each do |user|
@@ -40,15 +42,19 @@ User.all.each do |user|
   character = Character.find(character_id)
   user_character = user.add_character(character_id, Time.zone.now)
   character.user_characters << user_character
+  friends = User.where.not(id: user.id).shuffle.first(3)
+  friends.each do |friend|
+    _friend = Friendship.new(user: user, friend: friend)
+    if _friend.valid? 
+      _friend.save
+    end
+  end
 end
-
-friends = User.all.limit(3).second
-
-user.friends << friends
-
 
 from_date = Date.new(2018, 9, 1)
 to_date   = Date.new(2018, 9, 30)
+
+user3 = User.find_by!(nickname: 'ElRodra')
 
 IndividualHabit.create([
   {user_id: user.id, name: 'habito diario', description: 'diario', difficulty: 2, privacy: 1, frequency: 2,created_at: from_date},
@@ -56,6 +62,11 @@ IndividualHabit.create([
   {user_id: user.id, name: 'publico', description: 'publico', difficulty: 1, privacy: 1, frequency: 1},
   {user_id: user.id, name: 'protegido', description: 'protegido', difficulty: 2, privacy: 2, frequency: 1},
   {user_id: user.id, name: 'privado', description: 'privado', difficulty: 3, privacy: 3, frequency: 1},
+  {user_id: user3.id, name: 'habito diario', description: 'diario', difficulty: 2, privacy: 1, frequency: 2,created_at: from_date},
+  {user_id: user3.id, name: 'habito', description: 'habito', difficulty: 3, privacy: 1, frequency: 1, created_at: from_date},
+  {user_id: user3.id, name: 'publico', description: 'publico', difficulty: 1, privacy: 1, frequency: 1},
+  {user_id: user3.id, name: 'protegido', description: 'protegido', difficulty: 2, privacy: 2, frequency: 1},
+  {user_id: user3.id, name: 'privado', description: 'privado', difficulty: 3, privacy: 3, frequency: 1},
 ])
 
 habit = IndividualHabit.first
@@ -70,11 +81,25 @@ habit2 = IndividualHabit.second
 user2 = User.find_by!(nickname: 'Berna')
 
 group = Group.create(name: 'Propio', description: 'Propio Grupo', privacy: false)
+group2 = Group.create(name: 'Grupo de lectura', description: 'Esto es un grupo de lectura', privacy: false)
+group3 = Group.create(name: 'Grupo deamigas', description: '', privacy: false)
 
 membership = Membership.create(user_id: user.id, group_id: group.id, admin: true)
 membership2 = Membership.create(user_id: user2.id , group_id: group.id, admin: false)
 
+Membership.create(user: User.find_by_nickname('Pai'), group_id: group3.id, admin: true)
+Membership.create(user: User.find_by_nickname('ElRodra'), group_id: group3.id, admin: false)
+Membership.create(user: User.find_by_nickname('Berna'), group_id: group3.id, admin: false)
+Membership.create(user: user, group_id: group3.id, admin: false)
+
+Membership.create(user: User.find_by_nickname('ElRodra'), group_id: group2.id, admin: true)
+Membership.create(user: User.find_by_nickname('Lala'), group_id: group2.id, admin: false)
+Membership.create(user: User.find_by_nickname('Seba'), group_id: group2.id, admin: false)
+Membership.create(user: user, group_id: group2.id, admin: false)
+
 group_habit = GroupHabit.create(group_id: group.id, name: 'habito diario', description: 'diario', difficulty: 2, privacy: 1, frequency: 1,created_at: from_date)
+
+GroupHabit.create(group_id: group2.id, name: 'habito', description: 'diario', difficulty: 2, privacy: 1, frequency: 2,created_at: from_date)
 
 TrackGroupHabit.create(
   user_id: user.id,
