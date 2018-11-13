b28b96ec7085a4a201a827bfd24bd230c83e061b
Felipe de León <f7.deleon@gmail.com>
2018-11-13 01:03:27 -0300

    Fix paginacion grupos (#165)

--- app/controllers/me/groups_controller.rb
+++ app/controllers/me/groups_controller.rb
@@ -9,7 +9,7 @@ class Me::GroupsController < Me::ApplicationController
   def index
     options = {}
     options[:params] = { current_user: current_user }
-    groups = paginate current_user.groups, per_page: params['per_page'].to_i
+    groups = paginate current_user.groups.order('name ASC'), per_page: params['per_page'].to_i
     render json: GroupInfoSerializer.new(groups, options).serialized_json
   end
 
